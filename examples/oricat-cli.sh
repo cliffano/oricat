43a6780cc5aa36d64e99430bd5d2df2c59f72008
Cliffano Subagio <cliffano@gmail.com>
2024-05-25 09:54:16 +1000

    Add example testing.

--- examples/oricat-cli.sh
+++ examples/oricat-cli.sh
@@ -5,10 +5,11 @@ echo "Show help guide: oricat --help"
 oricat --help
 
 echo "\n\n========================================"
-echo "Run command with default config file: oricat"
-oricat
+echo "Prepare data"
+mkdir -p ../stage/examples/input/
+cp fixtures/*.* ../stage/examples/input/
 
 echo "\n\n========================================"
 echo "Run command with specified input output dirs:"
-echo "oricat --input-dir fixtures/ --output-dir ../stage/"
-oricat --input-dir fixtures/ --output-dir ../stage/
+echo "oricat --input-dir ../stage/examples/input/ --output-dir ../stage/output/"
+oricat --input-dir ../stage/examples/input/ --output-dir ../stage/examples/output/
