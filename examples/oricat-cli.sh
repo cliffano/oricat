713a170402bdc9078ae987bd6a496364e1f4a79c
Cliffano Subagio <cliffano@gmail.com>
2025-12-24 18:44:11 +0900

    Restructure examples dir.

--- examples/oricat-cli.sh
+++ examples/oricat-cli.sh
@@ -6,12 +6,16 @@ echo "\n\n========================================"
 echo "Show help guide: oricat --help"
 oricat --help
 
+printf "\n\n========================================\n"
+printf "Show version info: oricat --version\n"
+oricat --version
+
 echo "\n\n========================================"
 echo "Prepare data"
 mkdir -p ../stage/examples/input/
-cp fixtures/*.* ../stage/examples/input/
+cp fixtures/categorise/*.* ../stage/examples/input/categorise/
 
 echo "\n\n========================================"
 echo "Run command with specified input output dirs:"
-echo "oricat --input-dir ../stage/examples/input/ --output-dir ../stage/output/"
-oricat --input-dir ../stage/examples/input/ --output-dir ../stage/examples/output/
+echo "oricat categorise --input-dir ../stage/examples/input/categorise/ --output-dir ../stage/output/categorise/"
+oricat categorise --input-dir ../stage/examples/input/categorise/ --output-dir ../stage/output/categorise/
