2f32d6d649d7be8223a15a12f261fb3c0f233518
Pakkun Pakkun <blah+pakkun@cliffano.com>
2026-05-20 14:02:51 +1000

    Refactor: move categorise and blur-plates logic into separate modules

--- oricat/__init__.py
+++ oricat/__init__.py
@@ -1,99 +1,13 @@
-# pylint: disable=too-many-locals
 """
 oricat
 =======
 Tag any AWS resource via config file.
 """
 
-import os
 import click
-import cv2
-from PIL import Image
-from .logger import init
 
-
-def _categorise(input_dir: str, output_dir: str) -> None:
-    """Categorise image files based on orientation:
-    portrait, landscape, and square, one directory for each.
-    """
-
-    logger = init()
-
-    images = _read_images(input_dir, logger)
-
-    landscape_images, portrait_images, square_images = _categorise_images(
-        input_dir, images, logger
-    )
-
-    _write_images(landscape_images, "landscape", input_dir, output_dir, logger)
-    _write_images(portrait_images, "portrait", input_dir, output_dir, logger)
-    _write_images(square_images, "square", input_dir, output_dir, logger)
-
-
-def _read_images(input_dir: str, logger) -> list:
-    """Read images from the input directory."""
-
-    logger.info(f"Reading images from {input_dir}...")
-
-    exts = [".jpg", ".jpeg", ".png", ".gif"]
-    images = []
-    for image in os.listdir(input_dir):
-        if image.endswith(tuple(exts)):
-            logger.debug(f"Found {image}")
-            images.append(image)
-
-    logger.info(f"Found {len(images)} images in {input_dir}")
-
-    return images
-
-
-def _categorise_images(input_dir: str, images: list, logger) -> dict:
-    """Categorise images based on orientation: portrait, landscape, and square."""
-
-    logger.info(f"Categorising {len(images)} images...")
-
-    landscape_images = []
-    portrait_images = []
-    square_images = []
-
-    for image in images:
-        with Image.open(os.path.join(input_dir, image)) as img:
-            width, height = img.size
-            if width > height:
-                logger.debug(f"{image} is landscape")
-                landscape_images.append(image)
-            elif width < height:
-                logger.debug(f"{image} is portrait")
-                portrait_images.append(image)
-            else:
-                logger.debug(f"{image} is square")
-                square_images.append(image)
-
-    logger.info(f"Found {len(landscape_images)} landscape images")
-    logger.info(f"Found {len(portrait_images)} portrait images")
-    logger.info(f"Found {len(square_images)} square images")
-
-    return (landscape_images, portrait_images, square_images)
-
-
-def _write_images(
-    images: list, orientation: str, input_dir: str, output_dir: str, logger
-) -> None:
-    """Write images to the output directory based on the orientation as sub-directory."""
-
-    orientation_dir = os.path.join(output_dir, orientation)
-    logger.info(f"Writing {len(images)} {orientation} images to {orientation_dir}...")
-
-    if not os.path.exists(orientation_dir):
-        os.makedirs(orientation_dir)
-
-    for image in images:
-        logger.debug(f"Writing {image} to {orientation_dir}/")
-        os.rename(os.path.join(input_dir, image), os.path.join(orientation_dir, image))
-
-    logger.info(
-        f"Finished writing {len(images)} {orientation} images " f"to {orientation_dir}"
-    )
+from .blur_plates import _blur_plates
+from .categorise import _categorise
 
 
 @click.command()
@@ -115,47 +29,6 @@ def _write_images(
 def categorise(input_dir: str, output_dir: str) -> None:
     """Categorise image files by orientation."""
     _categorise(input_dir, output_dir)
-
-
-def _blur_plates(input_dir: str, output_dir: str) -> None:
-    """Detect and blur license plates in image files."""
-    # pylint: disable=no-member
-
-    logger = init()
-    plate_cascade_path = cv2.data.haarcascades + "haarcascade_russian_plate_number.xml"
-    plate_cascade = cv2.CascadeClassifier(plate_cascade_path)
-
-    if not os.path.exists(output_dir):
-        os.makedirs(output_dir)
-
-    exts = [".jpg", ".jpeg", ".png"]
-    images = [
-        f
-        for f in os.listdir(input_dir)
-        if os.path.isfile(os.path.join(input_dir, f))
-        and f.lower().endswith(tuple(exts))
-    ]
-
-    logger.info("Processing %s images from %s...", len(images), input_dir)
-
-    for filename in images:
-        input_path = os.path.join(input_dir, filename)
-        output_path = os.path.join(output_dir, filename)
-        logger.debug("Processing %s", filename)
-        img = cv2.imread(input_path)
-        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        plates = plate_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5)
-        if len(plates) > 0:
-            logger.info("Found %s plate(s) in %s, blurring...", len(plates), filename)
-            for x, y, w, h in plates:
-                roi = img[y : y + h, x : x + w]
-                img[y : y + h, x : x + w] = cv2.GaussianBlur(roi, (51, 51), 0)
-        else:
-            logger.warning("No plates detected in %s", filename)
-        cv2.imwrite(output_path, img)
-        logger.debug("Wrote %s", output_path)
-
-    logger.info("Finished processing %s images to %s", len(images), output_dir)
 
 
 @click.command()
---
+++ oricat/categorise.py
@@ -0,0 +1,93 @@
+"""Categorise module for oricat."""
+
+import os
+
+from PIL import Image
+
+from .logger import init
+
+
+def _read_images(input_dir: str, logger) -> list:
+    """Read images from the input directory."""
+
+    logger.info("Reading images from %s...", input_dir)
+
+    exts = [".jpg", ".jpeg", ".png", ".gif"]
+    images = []
+    for image in os.listdir(input_dir):
+        if image.endswith(tuple(exts)):
+            logger.debug("Found %s", image)
+            images.append(image)
+
+    logger.info("Found %s images in %s", len(images), input_dir)
+
+    return images
+
+
+def _categorise_images(input_dir: str, images: list, logger) -> tuple:
+    """Categorise images based on orientation: portrait, landscape, and square."""
+
+    logger.info("Categorising %s images...", len(images))
+
+    landscape_images = []
+    portrait_images = []
+    square_images = []
+
+    for image in images:
+        with Image.open(os.path.join(input_dir, image)) as img:
+            width, height = img.size
+            if width > height:
+                logger.debug("%s is landscape", image)
+                landscape_images.append(image)
+            elif width < height:
+                logger.debug("%s is portrait", image)
+                portrait_images.append(image)
+            else:
+                logger.debug("%s is square", image)
+                square_images.append(image)
+
+    logger.info("Found %s landscape images", len(landscape_images))
+    logger.info("Found %s portrait images", len(portrait_images))
+    logger.info("Found %s square images", len(square_images))
+
+    return (landscape_images, portrait_images, square_images)
+
+
+def _write_images(
+    images: list, orientation: str, input_dir: str, output_dir: str, logger
+) -> None:
+    """Write images to the output directory based on the orientation as sub-directory."""
+
+    orientation_dir = os.path.join(output_dir, orientation)
+    logger.info(
+        "Writing %s %s images to %s...", len(images), orientation, orientation_dir
+    )
+
+    if not os.path.exists(orientation_dir):
+        os.makedirs(orientation_dir)
+
+    for image in images:
+        logger.debug("Writing %s to %s/", image, orientation_dir)
+        os.rename(os.path.join(input_dir, image), os.path.join(orientation_dir, image))
+
+    logger.info(
+        "Finished writing %s %s images to %s", len(images), orientation, orientation_dir
+    )
+
+
+def _categorise(input_dir: str, output_dir: str) -> None:
+    """Categorise image files based on orientation:
+    portrait, landscape, and square, one directory for each.
+    """
+
+    logger = init()
+
+    images = _read_images(input_dir, logger)
+
+    landscape_images, portrait_images, square_images = _categorise_images(
+        input_dir, images, logger
+    )
+
+    _write_images(landscape_images, "landscape", input_dir, output_dir, logger)
+    _write_images(portrait_images, "portrait", input_dir, output_dir, logger)
+    _write_images(square_images, "square", input_dir, output_dir, logger)
